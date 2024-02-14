6aa6720c78d1f11fcf370001e002d1c85f0cea1b
tom38110 <tom.audard@gmail.com>
2024-02-14 21:01:03 +0100

    Capitole et monument aux morts

--- getimage.py
+++ getimage.py
@@ -1,3 +1,3 @@
 from bing_image_downloader import downloader
-downloader.download("capitole toulouse", limit=200,  output_dir='dataset', 
+downloader.download("monument aux morts toulouse", limit=200,  output_dir='dataset', 
                     adult_filter_off=True, force_replace=False, timeout=60)
