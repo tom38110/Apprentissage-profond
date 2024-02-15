e5f0dd89b5227f7512b4dbd1b471921778db352c
YoussefELALAMI <88277273+YoussefELALAMI@users.noreply.github.com>
2024-02-15 22:06:47 +0100

    Youssef - Mes Images (Pas encore trié)

--- getimage.py
+++ getimage.py
@@ -1,3 +1,3 @@
 from bing_image_downloader import downloader
-downloader.download("monument aux morts toulouse", limit=200,  output_dir='dataset', 
+downloader.download("hôtel d'Assezat toulouse", limit=200,  output_dir='Hotel_Assezat', 
                     adult_filter_off=True, force_replace=False, timeout=60)
