19c72152bc7f03d14efefe6a84b590090609d539
tom38110 <tom.audard@gmail.com>
2024-02-16 10:57:05 +0100

    Ajout Basilique saint-sernin

--- getimage.py
+++ getimage.py
@@ -1,3 +1,3 @@
 from bing_image_downloader import downloader
-downloader.download("monument aux morts toulouse", limit=200,  output_dir='dataset', 
+downloader.download("basilique saint-sernin de toulouse", limit=200,  output_dir='dataset', 
                     adult_filter_off=True, force_replace=False, timeout=60)
